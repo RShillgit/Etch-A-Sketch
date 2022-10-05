491db267a8e0413ed818cdf3e7c4e82e7597ac04
Robert Siciliano <Robert.siciliano22@gmail.com>
2022-10-05 12:51:32 -0400

    Resize Function

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Etch-A-Sketch
+Interact with this program by hovering your mouse over the grid to draw.  To resize the grid enter the dimensions you desire with an "x" inbetween. (ex: 20x20) 
--- scripts.js
+++ scripts.js
@@ -1,29 +1,47 @@
 // Container div that will hold all the boxes
 const grid = document.querySelector('.grid');
 
-// 16x16 grid 
+// Set initial grid dimensions to 16x16 
 let columns = 16;
 let rows = 16;
 
-for (let i = 0; i < columns; i++) {
-    // Create column divs
-    const column = document.createElement('div');
-    column.className = 'column';
-
-    for (let j = 0; j < rows; j++) {
-        // Create row divs
-        const row = document.createElement('div');
-        row.className = 'row';
-        row.setAttribute('id', 'box');
-        // Append rows to columns
-        column.appendChild(row);
+function createGrid(rows, columns) {
+    for (let i = 0; i < columns; i++) {
+        // Create column divs
+        const column = document.createElement('div');
+        column.className = 'column';
+    
+        for (let j = 0; j < rows; j++) {
+            // Create row divs
+            const row = document.createElement('div');
+            row.className = 'row';
+            row.setAttribute('id', 'box');
+            // Append rows to columns
+            column.appendChild(row);
+        };
+        // Append column to grid
+        grid.appendChild(column);
     };
-    // Append column to grid
-    grid.appendChild(column);
+}
+// Run this function on document load with the initial row/column values
+window.onload = createGrid(rows, columns);
+
+// Set the Grid Size input feild's placeholder to the current grid size
+const input = document.getElementById('size');
+input.setAttribute('placeholder', `${columns}x${rows}`);
+
+function resizeGrid() {
+    // Split input by the 'x' where the left value is rows and the right is columns
+    handledInput = input.value.split('x');
+    rows = handledInput[0];
+    columns = handledInput[1];
+
+    // Send values to the createGrid function
+    createGrid(rows, columns);
 };
 
-// When you hover over box, change its class to a diff name,
-// and change that class' background color in css
+
+// When you hover over a box, change its background color
 boxes = document.querySelectorAll('.row');
 
 boxes.forEach(box => {
